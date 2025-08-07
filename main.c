35ac86f846b8a99afa7f0568932a16a23255df71
awisse02@gmail.com <awisse02gmail.com>
2025-08-07 20:52:52 +0200

    slutgiltig lösning - inlämningsuppgift 1

--- main.c
+++ main.c
@@ -5,9 +5,29 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
+    int kast[100];
+    int antal[6] = {0}; // antal för 1-6
+    int summa = 0;
+    float medel;
 
-   
-    // Your code goes here
+    // slumpa 100 tärningskast och lagra i array
+    for (int i = 0; i < 100; i++ ){
+        kast[i] = (rand() % 6) + 1;
+        antal[kast[i] - 1]++;
+        summa += kast[i];
+    }
+    
+    //skriv antal för varje tärningsvärde 1-6
+    for (int i = 0; i < 6; i++) {
+        printf("%d\n", antal[i]);
+    }
+
+    //skriv summan av alla kast
+    printf("%d\n", summa);
+
+    //beräkna och skriva medelvärdet med 1 decimal
+    medel = (float)summa / 100;
+    printf("%.1f\n", medel);
 
     return 0;
 }
